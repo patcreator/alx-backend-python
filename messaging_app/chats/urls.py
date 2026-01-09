b8bae2b173ca967a80986e04c70a4ec63f6cc34e
patcreator <137688649+patcreator@users.noreply.github.com>
2026-01-09 12:26:53 +0200

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,7 +1,13 @@
-from django.contrib import admin
+from rest_framework import routers
 from django.urls import path, include
+from .views import ConversationViewSet, MessageViewSet
 
+# Create a router and register our viewsets
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
+
+# Include the router URLs in app-level urls
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')), 
+    path('', include(router.urls)),
 ]
