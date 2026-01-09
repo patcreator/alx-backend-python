92cbaa7c4a210f1160be9e5a35a34b8864aae49e
patcreator <137688649+patcreator@users.noreply.github.com>
2026-01-09 17:05:36 +0200

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -75,7 +75,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up class for integration tests"""
-        cls.get_patcher = patch('utils.requests.get')  # FIXED: patch utils.requests.get
+        cls.get_patcher = patch('utils.requests.get')
         cls.mock_get = cls.get_patcher.start()
 
         def side_effect(url):
