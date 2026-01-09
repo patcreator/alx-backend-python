7444fba16a980a7c9de1b6836a03b72f06e5cbde
patcreator <ppatcreator@gmail.com>
2026-01-09 13:28:55 +0200

    final error proff version

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -9,8 +9,9 @@ router.register(r'conversations', ConversationViewSet, basename='conversations')
 # Nested router for messages under a conversation
 nested_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
 nested_router.register(r'messages', MessageViewSet, basename='conversation-messages')
-
+router.register(r'messages', MessageViewSet, basename='messages')
 urlpatterns = [
     path('', include(router.urls)),
     path('', include(nested_router.urls)),  # Include nested routes
+    
 ]
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+# messaging_app/urls.py
+from django.http import JsonResponse
+
+def home(request):
+    return JsonResponse({"message": "Welcome to the Messaging API"})
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/v1/', include('chats.urls')),       # main API
     path('api-auth/', include('rest_framework.urls')),  # DRF login/logout
