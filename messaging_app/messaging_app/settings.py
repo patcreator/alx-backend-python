f9181ad8de1f5147528860a1ee2c20b799a77fa7
patcreator <ppatcreator@gmail.com>
2026-01-10 18:27:24 +0200

    new data

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -106,7 +106,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+    
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
+
+
 
 
 # JWT Settings
