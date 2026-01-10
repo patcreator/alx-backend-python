b74a2c207b20fb02dd49cbbe2f614e5873834a82
patcreator <ppatcreator@gmail.com>
2026-01-10 18:28:46 +0200

    new data

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,11 +1,17 @@
 # messaging_app/chats/pagination.py
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
-    """
-    TASK 2: Add pagination listing on the messages such that 
-    the api fetches 20 messages per page.
-    """
-    page_size = 20  # 20 messages per page
+    page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+    
+    # Make sure the response includes paginator.count
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,  # This line is important!
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
